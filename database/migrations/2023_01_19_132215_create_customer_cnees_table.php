114cd93d46ffd5e57f87d9fb1671d9f8ccd4ce68
juaniolivares95 <juaniolivares95@gmail.com>
2023-02-01 11:05:52 -0300

    migrate

--- database/migrations/2023_01_19_132215_create_customer_cnees_table.php
+++ database/migrations/2023_01_19_132215_create_customer_cnees_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('customer_cnees', function (Blueprint $table) {
             $table->id();
+            $table->string('razon_social');
+            $table->integer('tax_id');
+            $table->string('pais');
+            $table->string('provincia');
+            $table->string('mail');
+            $table->interger('phone');
+            $table->string('user');
+            $table->strign('empresa');
             $table->timestamps();
         });
     }
